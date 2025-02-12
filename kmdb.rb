50ebe671255e1c8cfa635d34de73d5c8bb75de5c
Pedro Rico <pedro.rico@kellogg.northwestern.edu>
2025-02-12 08:04:02 +0000

    Ex. Update (didn't working -> Check)

--- kmdb.rb
+++ kmdb.rb
@@ -76,14 +76,71 @@
 # Delete existing data, so you'll start fresh each time this script is run.
 # Use `Model.destroy_all` code.
 # TODO!
-Model.destroy_all
+Studio.destroy_all
+Movie.destroy_all
+Actor.destroy_all
+Role.destroy_all
+Rails.logger.info "------------------------"
+Rails.logger.info "----- FRESH START! -----"
+Rails.logger.info "------------------------"
 
 # Generate models and tables, according to the domain model.
 # TODO!
+puts "Done!"
 
 # Insert data into the database that reflects the sample data shown above.
 # Do not use hard-coded foreign key IDs.
 # TODO!
+new_studio = Studio.new
+new_studio["name"] = "Warner Bros"
+new_studio.save
+#
+new_actor = Actor.new
+new_actor["name"] = "Christian Bale"
+new_actor["name"] = "Michael Caine"
+new_actor["name"] = "Liam Neeson"
+new_actor["name"] = "Katie Holmes"
+new_actor["name"] = "Gary Oldman"
+new_actor.save
+#
+new_movie = Movie.new
+new_movie["title"] = "Batman Begins"
+new_movie["year_released"] = 2005
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = 1
+
+new_movie["title"] = "The Dark Knight"
+new_movie["year_released"] = 2008
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = 1
+
+new_movie["title"] = "The Dark Knight Rises"
+new_movie["year_released"] = 2012
+new_movie["rated"] = "PG-13"
+new_movie["studio_id"] = 1
+new_movie.save
+#
+new_role = Role.new
+new_role["movie_id"] = 1
+new_role["actor_id"] = 1
+new_role["character_name"] = "Bruce Wayne"
+
+new_role["movie_id"] = 1
+new_role["actor_id"] = 2
+new_role["character_name"] = "Alfred"
+
+new_role["movie_id"] = 1
+new_role["actor_id"] = 3
+new_role["character_name"] = "Ra's Al Ghul"
+
+new_role["movie_id"] = 1
+new_role["actor_id"] = 4
+new_role["character_name"] = "Rachel Dawes"
+
+new_role["movie_id"] = 1
+new_role["actor_id"] = 5
+new_role["character_name"] = "Commissioner Gordon"
+new_role.save
 
 # Prints a header for the movies output
 puts "Movies"
